d742e945104318bd87d128c68859237722eea83e
Barış ATALAY <baris.atalay@santsg.com>
2017-06-06 09:12:19 +0300

    Utils Method null parameter control

--- bluetoothhelper/src/main/java/com/atalay/bluetoothhelper/Common/UtilsGeneral.java
+++ bluetoothhelper/src/main/java/com/atalay/bluetoothhelper/Common/UtilsGeneral.java
@@ -23,6 +23,8 @@ public class UtilsGeneral {
     }
 
     public static byte[] bytePush(byte[] array, byte push) {
+        if(array == null) return new byte[0];
+
         byte[] longer = new byte[array.length + 1];
         System.arraycopy(array, 0, longer, 0, array.length);
         longer[array.length] = push;
@@ -31,10 +33,13 @@ public class UtilsGeneral {
 
     public static byte[] bytePushAll(byte[] array, byte[] push) {
         byte[] result;
+
         if(array == null)
             result = new byte[0];
         else
             result = array;
+
+        if(push == null) return result;
 
         for(int i=0;i<push.length;i++)
             result = bytePush(result,push[i]);
